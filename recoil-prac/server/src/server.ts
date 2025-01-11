98216704d3289ef652a467e483e7d7ccd0508f12
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-01-11 20:38:55 +0900

    [recoil-prac]chore: install and configure cors in server.ts

--- recoil-prac/server/src/server.ts
+++ recoil-prac/server/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import userRoutes from "./routes/users";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
