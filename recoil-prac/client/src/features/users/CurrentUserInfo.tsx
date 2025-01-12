d4d845726f8600013eeccb284339c8d508d4426f
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-01-12 17:13:41 +0900

    [recoil-prac]feat: create userNameQuery using selectorFamily for queries with parameters

--- recoil-prac/client/src/features/users/CurrentUserInfo.tsx
+++ recoil-prac/client/src/features/users/CurrentUserInfo.tsx
@@ -1,9 +1,34 @@
 import { useRecoilValue } from "recoil";
-// import { currentUserNameState } from "./states/currentUserNameState";
+import { currentUserNameState } from "./states/currentUserNameState";
 import { currentUserNameQuery } from "./states/currentUserNameQuery";
+import { userNameQuery } from "./states/userNameQuery";
+import { currentUserIDState } from "./states/currentUserIdState";
 
 export default function CurrentUserInfo() {
-  //   const userName = useRecoilValue(currentUserNameState);
-  const userName = useRecoilValue(currentUserNameQuery);
-  return <div>{userName}</div>;
+  const userId = useRecoilValue(currentUserIDState);
+  const userNameFromSyncFetch = useRecoilValue(currentUserNameState);
+  const userNameFromAsyncFetch = useRecoilValue(currentUserNameQuery);
+
+  return (
+    <div>
+      <p>
+        sync - id: {userId}, name: {userNameFromSyncFetch}
+      </p>
+      <p>
+        async - id: {userId}, name: {userNameFromAsyncFetch}
+      </p>
+      <UserInfo userId={1} />
+      <UserInfo userId={2} />
+      <UserInfo userId={3} />
+    </div>
+  );
+}
+
+function UserInfo({ userId }: { userId: number }) {
+  const userName = useRecoilValue(userNameQuery(userId));
+  return (
+    <div>
+      id: {userId}, name: {userName}
+    </div>
+  );
 }
