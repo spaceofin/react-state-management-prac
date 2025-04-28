1e158574ddcf52e02155d243cd6554cf71c07d15
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-04-28 22:30:37 +0900

    [hooks-prac]feat: add Header, set up routing, and create /use-state page

---
+++ hooks-prac/src/components/UseStateExample.tsx
@@ -0,0 +1,3 @@
+export default function UseStateExample() {
+  return <div>Hello, useState</div>;
+}
--- hooks-prac/src/main.tsx
+++ hooks-prac/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import Header from "./components/Header.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import UseStateExample from "./components/UseStateExample.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/use-state" element={<UseStateExample />} />
+    </Routes>
+  </BrowserRouter>
+  // </StrictMode>
 );
