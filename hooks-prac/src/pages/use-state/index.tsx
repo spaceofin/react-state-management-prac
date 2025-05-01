e65bc8a4f37b88b72f931de741e8b8ffe232e817
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-05-01 14:22:19 +0900

    [hooks-prac]feat: update use-state page to test useState and rerender behavior

--- hooks-prac/src/pages/use-state/index.tsx
+++ hooks-prac/src/pages/use-state/index.tsx
@@ -1,3 +1,35 @@
+import { useRef, useState } from "react";
+
 export default function UseStateExample() {
-  return <div>Hello, useState</div>;
+  const [count, setCount] = useState(0);
+  const countRef = useRef(-1);
+
+  countRef.current += 1;
+  console.log(countRef.current);
+
+  return (
+    <div className="py-14 px-16 flex flex-col gap-10">
+      <h1 className="text-4xl font-bold text-slate-600">
+        Rerender Count: {countRef.current}
+      </h1>
+      <span className="text-3xl text-slate-600">Count: {count}</span>
+      <div className="flex flex-col gap-5">
+        <button
+          className="px-4 py-3 text-2xl w-[600px] bg-blue-500 text-white rounded-xl hover:cursor-pointer"
+          onClick={() => setCount(1)}>
+          {`() => setCount(1) Click`}
+        </button>
+        <button
+          className="px-4 py-3 text-2xl  w-[600px] bg-blue-500 text-white rounded-xl hover:cursor-pointer"
+          onClick={() => setCount(count + 1)}>
+          {`() => setCount(count+1) Click`}
+        </button>
+        <button
+          className="px-4 py-3 text-2xl  w-[600px] bg-blue-500 text-white rounded-xl hover:cursor-pointer"
+          onClick={() => setCount((prev) => prev + 1)}>
+          {`() => setCount((prev) => prev + 1) Click`}
+        </button>
+      </div>
+    </div>
+  );
 }
