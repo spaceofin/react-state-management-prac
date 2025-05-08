afd2eef423adfa73781af1e1b3738e9789698a87
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-05-08 17:29:00 +0900

    [react-redux-prac]feat: add Header and set up routing

--- react-redux-prac/src/main.tsx
+++ react-redux-prac/src/main.tsx
@@ -4,11 +4,18 @@ import "./index.css";
 import App from "./App.tsx";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import Header from "./components/Header.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>
   // </StrictMode>,
 );
