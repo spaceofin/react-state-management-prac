e67a13127fdf17e61fc51b678491f41e561aaaea
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-05-08 19:11:42 +0900

    [react-redux-prac]feat: add /shop route

--- react-redux-prac/src/main.tsx
+++ react-redux-prac/src/main.tsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home.tsx";
 import App from "./App.tsx";
+import Shop from "./pages/Shop.tsx";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ react-redux-prac/src/pages/Shop.tsx
@@ -0,0 +1,3 @@
+export default function Shop() {
+  return <div className="p-10">This is Shop Page</div>;
+}
